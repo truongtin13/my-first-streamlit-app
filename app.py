a383a7cf1b0040761d48c346ecd3be52c589c49b
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-02-27 16:22:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
+a = st.number_input('Tham số a')
+b = st.number_input('Tham số b')
+if st.button('Giải'):
+  result = (-b)/(2*a)
+  st.success('Phương trình có 1 nghiệm',result)
