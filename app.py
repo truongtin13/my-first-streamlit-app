2d6a8958423e41e79a656c23da2d5263d14ac7cf
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-10 17:00:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
-  result = (-b)/(2*a)
-  st.success('Phương trình có 1 nghiệm',result)
+  if a ==0:
+    st.success('Phuongư trình vô nghiệm')
+  else:
+    result = (-b)/(2*a)
+    st.success('Phương trình có 1 nghiệm',result)
