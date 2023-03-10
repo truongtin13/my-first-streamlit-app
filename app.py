6c62fa23fd58494c44d9b6b3d46bfa7454e34cba
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-10 17:00:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
   if a ==0:
-    st.success('Phuongư trình vô nghiệm')
+    st.success('Phương trình vô nghiệm')
   else:
     result = (-b)/(2*a)
     st.success('Phương trình có 1 nghiệm',result)
