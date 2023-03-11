a0bf8bb88bed6d1c941915b1f3030f716ee18a29
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-11 14:32:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import sklearn
 st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
-  if a ==0:
-    st.success('Phương trình vô nghiệm')
+  if a == 0:
+    if b == 0:
+      st.success('Phương trình vô số nghiệm')
+    else:
+      st.success('Phương trình vô nghiệm')
   else:
     result = (-b)/(2*a)
     st.success('Phương trình có 1 nghiệm',result)
