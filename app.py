ddbabc6ac2d028311611809b7ff22539d0cccc77
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2024-05-17 16:11:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import hmac
 import streamlit as st
 
-password = "huutin0908200471"
-check = st.text_input("Please enter your password:")
-if check != password:
-    st.write('False')
-else:
-    st.write('True')
+st.write('Hello')
