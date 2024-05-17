3cc521da753c7945b56a41951726bb4ec8350ee3
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2024-05-17 16:04:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 import hmac
 import streamlit as st
 
-def check_password():
-    def password_entered():
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"] 
-        else:
-            st.session_state["password_correct"] = False
-    if st.session_state.get("password_correct", False):
-        return True
-
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
-
-
-if not check_password():
-    st.stop() 
-
-st.write('Hello')
-
+password = "huutin0908200471"
+check = st.text_input("Please enter your password:")
+if check != password:
+    st.write('False')
+else:
+    st.write('True')
